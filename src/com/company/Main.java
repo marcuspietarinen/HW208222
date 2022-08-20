7b680bf539b3fc3fb6ea4934a35e1574ca0e386b
Marcus Pietarinen <marcuspietarinen@Lauris-MacBook-Pro.local>
2022-08-20 20:07:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int a = 8;
+        int b = 8;
+
+        if (a == b)
+            System.out.println((a + b)*(a + b));
+        else
+            System.out.println(a + b);
+    }
+}
